2d2a16ab8ccfd80833891b22bef2d21205e79e9d
Nathan Waters <nathan@nathanwaters.com>
2014-11-17 18:55:52 +1100

    FAQs

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,6 +99,24 @@ $(function() {
         }
       ]
     });
+    
+    //blog slider
+    $('#blog-slider').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
 
     //video
     var tag = document.createElement('script');
